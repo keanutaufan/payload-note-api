e46cf6b0bd88d4d87a6944a723de6ddd6dc3ec02
keanutaufan <keanu.taufan21@gmail.com>
2023-11-08 19:17:26 +0700

    protect admin identity

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,10 +1,17 @@
 import { CollectionConfig } from 'payload/types'
+import { adminOnly } from '../access/adminOnly'
 
 const Users: CollectionConfig = {
   slug: 'users',
   auth: true,
   admin: {
     useAsTitle: 'email',
+  },
+  access: {
+    create: adminOnly,
+    read: adminOnly,
+    update: adminOnly,
+    delete: adminOnly,
   },
   fields: [
     // Email added by default
